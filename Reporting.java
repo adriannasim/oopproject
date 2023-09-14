029a8a4721ea39f08d6eafe00701f028fc5dccb1
tzwynn02 <wynntze@gmail.com>
2023-09-15 03:23:46 +0800

    .

---
+++ Reporting.java
@@ -0,0 +1,17 @@
+public class Reporting {
+    private FoodAndBeverage[] fndSold;
+    private Ticket[] ticketSold;
+
+    public double calculateFnbSales(FoodAndBeverage[] fnb){
+       // return
+    }
+
+    public double fnbDailySales(FoodAndBeverage[] fnb){
+        
+    }
+
+    public double calculateTicketSales(Ticket[] ticketSold){
+        
+    }
+
+}
