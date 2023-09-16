1a70edd0aa12067304f513154085c35100da798b
adriannasim <adriannasim@gmail.com>
2023-09-17 00:59:06 +0800

    adr

--- Login.java
+++ Login.java
@@ -1,3 +1,31 @@
 public class Login {
+    //variable declaration
+    private String username;
+    private String password;
+
+    //constructors
+    Login() {
+        this.username = "";
+        this.password = "";
+    }
+    Login(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
     
+    //setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //getters
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword(String username) {
+        return password;
+    }
 }
