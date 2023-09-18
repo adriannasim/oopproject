bcfc2d4000cf772d569a982ec99ac3c957a4cb8a
tzwynn02 <wynntze@gmail.com>
2023-09-19 02:25:56 +0800

    update

--- Reporting.java
+++ Reporting.java
@@ -1,17 +1,23 @@
 public class Reporting {
-    private FoodAndBeverage[] fndSold;
+    private FoodAndBeverage[] fnbSold;
     private Ticket[] ticketSold;
 
-    public double calculateFnbSales(FoodAndBeverage[] fnb){
-       // return
-    }
-
-    public double fnbDailySales(FoodAndBeverage[] fnb){
+    public double calculateFnbSales(FoodAndBeverage[] fnbSold){
         
+    }
+    public double fnbDailySales(FoodAndBeverage[] fnb){
+        double totalFnb = 0;
+        for(int i = 0;i <= fnb.length;i++){
+            totalFnb += fnbSold[i].getFoodPrice();
+        }
+        return totalFnb;
     }
 
     public double calculateTicketSales(Ticket[] ticketSold){
-        
+        double totalTicketSales = 0;
+        for (int i = 0; i < ticketSold.length; i++) {
+            totalTicketSales += ticketSold[i].getTicketPrice();
+        }
+        return totalTicketSales;
     }
-
-}
+    }
