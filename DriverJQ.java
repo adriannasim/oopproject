9d1e7a6ebdf6eac17c1dfe8be0cc28c20e6594a5
Junqing23 <sawjq-pm22@student.tarc.edu.my>
2023-09-20 11:33:17 +0800

    up

--- DriverJQ.java
+++ DriverJQ.java
@@ -2,17 +2,16 @@
 import java.util.*;
 import java.time.LocalDate;
 
-
 // Delete entire driverJQ if donee
 
 public class DriverJQ {
 
     public static void main(String[] args) throws Exception {
-        //viewReport();
-        //viewReportDDrink();
-        //viewReportTicket();
+        viewReport();
+        viewReportDDrink();
+        viewReportTicket();
 
-        Purchase.makePurchase(1, new Login("username", "password"));
+        //Purchase.makePurchase(2, new Login("username", "password"));
     }
 
     public static void viewReport() throws Exception {
@@ -20,11 +19,13 @@ public class DriverJQ {
         ArrayList<Snacks> purchaseSnack = Purchase.readFromSnackFile("purchaseSnack.txt");
         ArrayList<String> snackCust = Purchase.snackCust;
         ArrayList<Double> snackTotalPrice = Purchase.snackTotalPrice;
+        ArrayList<LocalDate> snackDates = Purchase.snackDates;
 
         int i = 0;
         for (Snacks snack : purchaseSnack) {
             System.out.printf("%d. ", i + 1);
-            System.out.println(snack.displayToReport());
+            System.out.println(snack.displayToCust());
+            System.out.println(snackDates.get(i));
             System.out.println(snackCust.get(i));
             System.out.println(snackTotalPrice.get(i));
             i++;
@@ -37,11 +38,13 @@ public class DriverJQ {
         ArrayList<Drinks> purchaseDrink = Purchase.readFromDrinkFile("purchaseDrink.txt");
         ArrayList<String> drinkCust = Purchase.drinkCust;
         ArrayList<Double> drinkTotalPrice = Purchase.drinkTotalPrice;
+        ArrayList<LocalDate> drinkDates = Purchase.drinkDates;
 
         int i = 0;
         for (Drinks drink : purchaseDrink) {
             System.out.printf("%d. ", i + 1);
             System.out.println(drink);
+            System.out.println(drinkDates.get(i));
             System.out.println(drinkCust.get(i));
             System.out.println(drinkTotalPrice.get(i));
             i++;
--- Snacks.java
+++ Snacks.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
 import java.io.Writer;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -71,11 +72,13 @@ public class Snacks extends FoodAndBeverage implements Serializable {
 
     // WRITE PURCHASE INTO FILE
     public void writePurchaseFnB(FoodAndBeverage fnb, String username) {
+        LocalDate today = LocalDate.now();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("purchaseSnack.txt", true))) {
             writer.write(username + "||" +
                     fnb.getFoodId() + "||" + fnb.getClass() + "||"
                     + fnb.getFoodName() + "||" + fnb.getFoodPrice() + "||"
-                    + fnb.getPurchaseQty() + "||" + fnb.calculatePrice());
+                    + fnb.getPurchaseQty() + "||" + fnb.calculatePrice() + "||"
+                    + today.getDayOfMonth() + "||" + today.getMonthValue() + "||" + today.getYear());
             writer.newLine();
         } catch (IOException e) {
             e.printStackTrace();
